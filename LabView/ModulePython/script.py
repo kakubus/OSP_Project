46bc84b37dd85fa2b77a0818d5da815c2fecea65
pwojx <pawewoj883@student.polsl.pl>
2021-03-12 17:19:21 +0100

    Fix relative path problem

--- LabView/ModulePython/script.py
+++ LabView/ModulePython/script.py
@@ -1,2 +1,5 @@
-def adding(a,b):
-    return a+b
+# Podajecie trzy argumenty poprzez LabView,
+# wiec tutaj tez musza byc 3, bo inaczej
+# Python sie wkurzy ze dostal 3, a chcial 2.
+def process_image(a, b, c):
+    return a + b
